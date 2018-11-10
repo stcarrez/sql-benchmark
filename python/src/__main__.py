da92eed189dad3badce222c1446340cf4a5581ed
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-11-10 21:51:15 +0100

    Add option -mysql and support for MySQL benchmark

--- python/src/__main__.py
+++ python/src/__main__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-SQLite Benchmark tests
+SQL Benchmark tests
 """
 __author__ = "Stephane Carrez"
 __copyright__ = "Copyright (C) 2018 Stephane Carrez"
@@ -12,12 +12,15 @@ import sqlite_benchmark
 import sqlite_simple
 import postgresql_benchmark
 import postgresql_simple
+import mysql_benchmark
+import mysql_simple
 import sys
 
 parser = argparse.ArgumentParser(description='SQL Benchmark')
 parser.add_argument('-r', dest='repeat', help='Repeat counter', type=int)
 parser.add_argument('-sqlite', help='Run the SQLite benchmarks', action="store_true")
 parser.add_argument('-postgresql', help='Run the PostgreSQL benchmarks', action="store_true")
+parser.add_argument('-mysql', help='Run the Mysql benchmarks', action="store_true")
 parser.add_argument('-o', dest='output', help='Write the result in the file')
 
 if __name__ == '__main__':
@@ -35,8 +38,12 @@ if __name__ == '__main__':
         postgresql_benchmark.PostgreSQLBenchmark.setup()
         tests = postgresql_simple.create()
 
+    if result.mysql:
+        mysql_benchmark.MysqlBenchmark.setup()
+        tests = mysql_simple.create()
+
     if not tests:
-        print("Missing -sqlite or -postgresql option", file=sys.stderr)
+        print("Missing -sqlite, -postgresql or -mysql option", file=sys.stderr)
         sys.exit(1)
 
     for test in tests:
